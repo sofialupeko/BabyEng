7a01bac4660aa86f9bd59825bdf295c04ac7ff33
Sofia Lupeko <sonya.lupeko@mail.ru>
2021-10-13 21:52:37 +0300

    Creation

---
+++ BabyEng/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BabyEng
+//
+//  Created by Denis Valshchikov on 13.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
